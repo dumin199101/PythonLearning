097ce9795ccd8c135ebe3e62a7ed4f5a26a079aa
dumin199101 <dumin199101@163.com>
2017-07-26 22:43:54 +0800

    Python逻辑运算符

---
+++ itcast_base/day03/05IfDemo.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+a = 100
+if a==100:
+    print u"a等于100"
+else:
+    print u"a不等于100"
+
+#逻辑运算符
+b = 1
+c = 1
+if b and c:
+    print "b and c both 1"
+
+d = 0
+if not d:
+    print "d is 0"
+
+if a or d:
+    print u"一真则为真"

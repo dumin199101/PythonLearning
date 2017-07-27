b0fdca2063adf1b858c198a0ed70e352206be77a
dumin199101 <dumin199101@163.com>
2017-07-27 22:11:57 +0800

    Python for循环

--- itcast_base/day03/06WhileDemo.py
+++ itcast_base/day03/06WhileDemo.py
@@ -3,3 +3,10 @@ i=1
 while i<10:
     print "Hello Python"
     i+=1
+
+math = 100
+english = 99
+print("my math score is %03d,my english score is %03d ."%(math,english))
+
+j = 5
+print("j is %02d"%j)
---
+++ itcast_base/day03/07ForDemo.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+import time
+name = "dumin"
+for tmp in name:
+    print("tmp is %s"%tmp)
+    time.sleep(1)

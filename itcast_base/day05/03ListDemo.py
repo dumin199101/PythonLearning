513ecd80de2e0543efd192eb706d3d622273e11d
dumin199101 <dumin199101@163.com>
2017-09-24 12:36:22 +0800

    列表的增删改查操作

---
+++ itcast_base/day05/03ListDemo.py
@@ -0,0 +1,45 @@
+#coding:utf-8
+def scanList(name_list):
+    for temp in name_list:
+        print temp
+name_list = ['wangliang','sunjun','zhanghui','qinfeng']
+print u"原始数据"
+scanList(name_list)
+name_list.append('sunwen')
+print u"追加数据:append"
+scanList(name_list)
+print u"追加数据：insert"
+name_list.insert(1,"dumin")
+scanList(name_list)
+age_list = [22,33,44]
+print u"追加数据：extend"
+name_list.extend(age_list)
+print name_list #['wangliang', 'dumin', 'sunjun', 'zhanghui', 'qinfeng', 'sunwen', 22, 33, 44]
+
+name_list[0] = 'haoshuai'
+print u"修改第一个元素："
+scanList(name_list)
+
+print u"查找元素："
+tmp = 'qinfeng'
+if tmp in name_list:
+    print "In"
+else:
+    print "not In"
+print name_list.index('sunwen',0)
+print name_list.count('sunwen')
+
+print u"删除元素"
+movie_list = ["速度与激情","变形金刚","霍比特人","战狼"]
+del movie_list[0]
+print u"通过del方式删除："
+for temp in movie_list:
+    print temp.decode('utf-8')
+print u"通过pop方式删除："
+movie_list.pop()
+for temp in movie_list:
+    print temp.decode('utf-8')
+print u"通过remove方式删除："
+movie_list.remove('变形金刚')
+for temp in movie_list:
+    print temp.decode('utf-8')

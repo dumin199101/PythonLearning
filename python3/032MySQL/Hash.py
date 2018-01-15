e97d9a453853436f329b20a6cff909be820cc4c0
lieyan123091 <1766266374@qq.com>
2018-01-15 10:43:47 +0800

    Python中的hashlib模块：sha1加密跟md5加密

--- python3/032MySQL/Hash.py
+++ python3/032MySQL/Hash.py
@@ -1,4 +1,10 @@
 import hashlib
+# md5加密
 hash = hashlib.md5()
 hash.update("Hello world".encode("utf-8"))
 print(hash.hexdigest())
+
+# sha1加密
+sha1 = hashlib.sha1()
+sha1.update("Hello world".encode("utf-8"))
+print(sha1.hexdigest())

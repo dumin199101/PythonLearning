d4a98e7757dc103567571bc17eb9dc8c749dbc99
lieyan123091 <1766266374@qq.com>
2018-01-24 16:43:17 +0800

    Django模型类生成数据

---
+++ DjangoLearning/blog/myblog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField()
+    def __str__(self):
+        return self.btitle
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=1000)
+    hbook = models.ForeignKey(BookInfo,on_delete=models.CASCADE)

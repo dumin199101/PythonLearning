a871108c85ddcb9f2beb72c0412c05c07f39a9e4
lieyan123091 <1766266374@qq.com>
2018-03-05 16:14:49 +0800

    Django模型详解之管理器

--- DjangoLearning/test1/booktest/models.py
+++ DjangoLearning/test1/booktest/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from datetime import datetime
+
+class BookInfoManager(models.Manager):
+   def get_queryset(self):
+       return super().get_queryset().filter(isDelete=False)
+
+   def create(self,btitle,bpub_date):
+       b = BookInfo()
+       b.btitle = btitle
+       b.bpub_date = bpub_date
+       b.bread = 0
+       b.bcomment = 0
+       b.isDelete = 1
+       return b
 
 # Create your models here.
 class BookInfo(models.Model):
@@ -9,6 +23,20 @@ class BookInfo(models.Model):
     isDelete = models.BooleanField(default=False)
     class Meta:
         db_table = 'book_info'
+    books1 = models.Manager()
+    books2 = BookInfoManager()
+
+    @classmethod
+    def create(cls,btitle,bpub_date):
+        b = BookInfo()
+        b.btitle = btitle
+        b.bpub_date = bpub_date
+        b.bread = 0
+        b.bcomment = 0
+        b.isDelete = 1
+        return b
+
+
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=100)
